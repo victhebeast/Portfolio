e5bbce239ed06fc5f6973db2abaf652b3cbc005d
demosjarco <demosjarco@users.noreply.github.com>
2019-04-10 20:19:09 -0700

    created test template

---
+++ html/test.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+// HTTP2 Push
+header("Link: </css/main.css>; as=style; rel=preload", false);
+header("Link: </css/light.css>; as=style; rel=preload", false);
+header("Link: </css/dark.css>; as=style; rel=preload", false);
+header("Link: </css/about.css>; as=style; rel=preload", false);
+header("Link: </images/close-up-code-codes-239898.jpg>; rel=preload; as=image", false);
+header("Link: </images/logo.svg>; rel=preload; as=image", false);
+header("Link: <//use.fontawesome.com/releases/v5.8.1/css/all.css>; rel=preload; as=image", false);
+?>
+<!doctype html>
+<html>
+<head>
+	<?php include_once("./parts/head.html") ?>
+	<title>Template</title>
+	<link rel="stylesheet" href="css/about.css" type="text/css" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+
+<body class="light">
+	<?php include_once("./parts/header.html") ?>
+	<div class="card">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam veritatis iusto nobis repudiandae iure sapiente corrupti reprehenderit delectus voluptatibus vero voluptas distinctio, ipsa nostrum. Error illum ea porro quasi adipisci?</p>
+		<footer><i class="fab fa-facebook-f"></i><i class="fab fa-twitter"></i><i class="fab fa-linkedin-in"></i><i class="fab fa-github"></i></footer>
+	</div>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias obcaecati illo beatae suscipit facilis sit velit, voluptatem laborum, dicta, at accusantium. Autem minima, iusto molestias voluptas expedita maxime officia commodi dolore. Sapiente, veniam autem maxime omnis totam eos pariatur labore commodi, optio numquam quis dignissimos. Enim rerum aut eum voluptatibus, consequuntur unde, suscipit. Sed quas, iure architecto rem facere sit voluptates eaque accusantium eligendi debitis laudantium eum minus, iste vero corporis sapiente, veniam nisi itaque eos in nobis obcaecati. Natus facere, porro ea sit vero exercitationem! Impedit expedita, harum quae repellendus dicta sapiente, nostrum odit exercitationem ullam. Eligendi, necessitatibus, totam?</p>
+	<p>Reiciendis repellat temporibus, sunt ab odio nihil, commodi. Aperiam accusantium quos consequatur facere iusto repellat aliquam laudantium tenetur voluptas ab dolore sed molestias molestiae nesciunt, aut deleniti maiores. Tempora consequatur quisquam excepturi ex tempore nostrum nisi officiis. Dicta quibusdam, mollitia ipsa, quisquam omnis esse dolores similique eum quas nesciunt, modi rem accusamus, harum. Ab deleniti consectetur recusandae. Dolores quasi harum provident doloremque ea, aliquid doloribus, optio laborum. Cum, qui soluta id expedita obcaecati non eum corporis culpa voluptatum consequuntur, iure eligendi doloremque nisi impedit voluptas exercitationem labore ad delectus sit asperiores. Minima accusamus saepe eligendi ea adipisci quam, vero. Cum!</p>
+	<p>Quod sunt laborum pariatur aspernatur ullam ipsam nulla, eius natus ex commodi nisi, non nemo. A neque nesciunt, enim, aut at, eaque reprehenderit tempora eos facilis impedit, temporibus asperiores. Corporis optio quia quis itaque quas ut quo amet! Maxime recusandae molestias placeat dolorum inventore cum quidem cumque! Eveniet culpa fuga laudantium molestias blanditiis corporis hic tempora, nobis, enim optio itaque vel harum ipsum, amet architecto voluptatum explicabo. A asperiores ratione, dolores, temporibus reprehenderit, ab expedita fugit consectetur sunt minima non ipsa debitis. Sequi saepe voluptates maxime quis quasi officia cum debitis amet fugiat, praesentium odit repellat suscipit voluptate assumenda, obcaecati!</p>
+	<p>Ullam ut, incidunt minus sunt harum nemo ratione, voluptatem eum possimus rerum quas odit expedita reprehenderit quo neque a dolor vel velit illo rem unde! Fugit consequuntur laboriosam iste optio cum nesciunt praesentium voluptatem repellat ex repudiandae labore alias placeat vero, velit molestiae esse asperiores quibusdam aliquid necessitatibus suscipit a impedit. Cum rerum ea vel at repudiandae odio reiciendis id beatae sunt. Deserunt beatae voluptates, perspiciatis. Nulla sapiente, iusto, modi numquam accusantium tenetur delectus quod similique. Laboriosam placeat quos consequatur temporibus fugit harum, praesentium necessitatibus doloremque vero! Labore tenetur totam, tempore maiores? Cumque rerum reiciendis omnis facilis doloremque laboriosam odio!</p>
+	<p>Ipsam, laudantium, officia. Cumque repudiandae, quas, rerum quo, consequatur, corporis perferendis accusantium inventore laborum ea eos odit facere. Nemo tempore mollitia, consequatur aliquid, quis porro nihil enim voluptatem amet fugiat facere, provident ducimus perferendis distinctio eaque voluptate quod. A minus placeat voluptate nobis eum eligendi tempore quisquam inventore dolorem molestiae illo aspernatur distinctio aperiam tenetur numquam, repellendus et suscipit facilis aut voluptatibus nam beatae quod? Qui aperiam in error vel blanditiis illo modi unde labore exercitationem cupiditate quos, nesciunt tenetur eos laboriosam saepe illum nostrum ullam quas praesentium iusto magni id! Voluptates alias veritatis voluptatibus, repudiandae illum, dolorem quas dignissimos!</p>
+	<?php include_once("./parts/footer.html") ?>
+</body>
+</html>
