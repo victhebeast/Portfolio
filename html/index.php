112b79bcf019d5ac6a76ea19c87ae63f450edb9f
demosjarco <demosjarco@users.noreply.github.com>
2019-04-10 20:27:56 -0700

    created base html page

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+// HTTP2 Push
+header("Link: </css/main.css>; as=style; rel=preload", false);
+header("Link: </css/light.css>; as=style; rel=preload", false);
+header("Link: </css/dark.css>; as=style; rel=preload", false);
+header("Link: </css/about.css>; as=style; rel=preload", false);
+header("Link: </images/close-up-code-codes-239898.jpg>; rel=preload; as=image", false);
+header("Link: </images/logo.svg>; rel=preload; as=image", false);
+header("Link: <https://use.fontawesome.com/releases/v5.8.1/css/all.css>; rel=preload; as=image; crossorigin", false);
+header("Link: <https://fonts.googleapis.com/css?family=Lato:400,700|Open+Sans:400,700|Roboto:400,700>; rel=preload; as=image; crossorigin", false);
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
