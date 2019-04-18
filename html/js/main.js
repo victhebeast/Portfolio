a8b9e8d970dbc4b9d65eb3cc1a8a227962bc17bf
demosjarco <demosjarco@users.noreply.github.com>
2019-04-17 21:23:06 -0700

    delay no longer needed since background image url()

--- html/js/main.js
+++ html/js/main.js
@@ -1,6 +1,4 @@
-// Load background image async since big
 $(function() {
-	setTimeout(() => {
-		$('header div.background').css('background-image', 'url("../images/close-up-code-coding-239898.png")');
-	}, 10);
+	// Load background image async since big
+	$('header div.background').css('background-image', 'url("../images/close-up-code-coding-239898.png")');
 });
