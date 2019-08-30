5453d86b3d12bf8f70f3ee9dfd405d100bb25f16
demosjarco <demosjarco@users.noreply.github.com>
2019-08-29 19:09:24 -0700

    Revert to original dank beats

--- html/dankbeats.php
+++ html/dankbeats.php
@@ -1,111 +1,85 @@
-<?php declare(strict_types=1);
-// HTTP2 Push
-include_once("./parts/preload.php");
-header("Link: </css/about.css>; as=style; rel=preload", false);
-header("Link: </css/dankbeats.css>; as=style; rel=preload", false);
-
-$fileName = basename(__FILE__, '.php');
-?>
 <!doctype html>
-<html><!-- InstanceBegin template="/Templates/main.dwt" codeOutsideHTMLIsLocked="false" -->
+<html>
 <head>
-<?php include_once("./parts/head.html") ?>
-<!-- InstanceBeginEditable name="doctitle" -->
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="mobile-web-app-capable" content="yes" />
 <title>Dank Beats</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<link rel="manifest" href="dankbeats/manifest.json" type="application/json" />
-<link rel="stylesheet" href="css/about.css" type="text/css" />
-<link rel="stylesheet" href="css/dankbeats.css" type="text/css" />
-<!-- InstanceEndEditable -->
+<link rel="stylesheet" href="main.css" type="text/css" />
 </head>
 
-<body class="light">
-	<?php include_once("parts/header.php") ?>
+<body>
+<img id="logo" src="img/DankBeats.png" />
+<div class="segment" style="background-color: #FF8F00;">
+	<div id="profile" style="background-color: #7289da;">
+		<div id="profileIcon"></div>
+		<span id="botName"><span style="font-weight: bold; font-size: larger;">Dank Beats</span><span style="font-size: smaller; color: rgba(255,255,255,0.75)">#0979</span><span id="botTag">Bot</span></span>
+	</div>
+	<div id="profileContent">
+		<span id="discordButton"><a href="https://discordapp.com/oauth2/authorize?client_id=281635696559652864&scope=bot&permissions=3148800" target="_blank">Add to <img src="https://discordapp.com/assets/93608abbd20d90c13004925014a9fd01.svg" style="height: 38px; vertical-align: text-bottom;" /></a></span>
+	</div>
+</div>
+<div class="segment">
+	<div id="quality"></div>
+	<div id="qualityContent">
+		<h2>Quality</h2>
+		<p>Why get a music bot that plays music in bad quality? Other discord music bots play music in 32-64kpbs or even lower. We give you all the bits you deserve! Whatever your bitrate is, even if you have 128kpbs because you're a partnered server, we will match it and play it in all its glory. If you change the quality after it its playing, just redo the play command.</p>
+	</div>
+</div>
+<div class="segment">
+	<div id="edm">
+		<div style="background-image: url(https://dm3381rcqf07k.cloudfront.net/multisite.insomniac.com/wp-content/uploads/sites/28/2017/04/updated_edc_logo1.png);"></div>
+		<div style="background-image: url(https://www.bonnaroo.com/wp-www-bonnaroo-com/wp/wp-content/uploads/2016/11/logo3.png);"></div>
+		<div style="background-image: url(https://s3.amazonaws.com/hardfest/wp-content/uploads/2016/04/hsmfwhite.png);"></div>
+		<div style="background-image: url(https://www.lollapalooza.com/wp-www-lollapalooza-com/wp/wp-content/uploads/2014/05/lolla-logo.png);"></div>
+	</div>
+	<div id="edmContent">
+		<div id="background"></div>
+		<h2>EDM Festivals</h2>
+		<p>Replay the live sets played by famous artists from the TOP EDM festivals in the world. Enjoy 1+ hour sets from artists such as Dillon Francis, Alison Wonderland, Marshmellow, and more. You can search by artist, event, year, and/or location. Just use <span class="code">!beats edm &lt;search terms&gt;</span>.</p>
+	</div>
+</div>
+<div class="segment">
+	<div id="radio">
+		<div id="radioIcon"></div>
+	</div>
+	<div id="radioContent">
+		<h2>Live Radio</h2>
+		<p>We have handpicked 5 radio station that we enjoy that provide live dj-ing/mixing. Just use <span class="code">!beats play &lt;station number&gt;</span>. To see full list, use <span class="code">!beats help</span>. You may visit their sites below by clicking on their logo or even listen to them with the player below. You can also send us more stations to add by <a href="//techgeniusapps.com/contact.php" target="_self">contacting us</a>.</p>
+	</div>
+</div>
+<div id="container">
 	<div class="card">
-		<!-- InstanceBeginEditable name="card" -->
-		<img src="dankbeats/img/DankBeats.png" style="width: 100%;" />
-		<!-- InstanceEndEditable -->
+		<a href="http://power983.com/" target="_blank"><img src="img/top-logo.png" class="logo"></a>
+		<audio controls>
+			<source src="http://ice42.securenetsystems.net/KKFR" type="audio/mpeg" />
+		</audio>
 	</div>
-	<!-- InstanceBeginEditable name="Main Body" -->
-	<div class="container">
-		<div class="row">
-			<div class="col-lg">
-				<div class="card">
-					<div class="card-body">
-						<div id="profile" style="background-color: #7289da;">
-							<div id="profileIcon"></div>
-							<span id="botName"><span style="font-weight: bold; font-size: larger;">Dank Beats</span><span style="font-size: smaller; color: rgba(255,255,255,0.75)">#0979</span><span id="botTag">Bot</span></span>
-						</div>
-						<a href="https://discordapp.com/oauth2/authorize?client_id=281635696559652864&scope=bot&permissions=3148800" class="btn btn-primary">Add to DISCORD</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg">
-				<div class="card">
-					<img src="dankbeats/img/bitrate.png" class="card-img-top" />
-					<div class="card-body">
-						<h5 class="card-title">Quality</h5>
-						<p class="card-text">Why get a music bot that plays music in bad quality? Other discord music bots play music in 32-64kpbs or even lower. We give you all the bits you deserve! Whatever your bitrate is, even if you have 128kpbs because you're a partnered server, we will match it and play it in all its glory. If you change the quality after it its playing, just redo the play command.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg">
-				<div class="card bg-dark text-white">
-					<img src="https://dm3381rcqf07k.cloudfront.net/multisite.insomniac.com/wp-content/uploads/sites/28/2017/04/updated_edc_logo1.png" class="card-img-top fit" />
-					<img src="https://www.bonnaroo.com/wp-www-bonnaroo-com/wp/wp-content/uploads/2016/11/logo3.png" class="card-img-top fit" />
-					<img src="https://d3vhc53cl8e8km.cloudfront.net/hello-staging/wp-content/uploads/sites/9/2018/03/08174606/hard-logo-white-compressor.svg" class="card-img-top fit" />
-					<img src="https://www.lollapalooza.com/wp-www-lollapalooza-com/wp/wp-content/uploads/2014/05/lolla-logo.png" class="card-img-top fit" />
-					<div class="card-body">
-						<h5 class="card-title">EDM Festivals</h5>
-						<p class="card-text">Replay the live sets played by famous artists from the TOP EDM festivals in the world. Enjoy 1+ hour sets from artists such as Dillon Francis, Alison Wonderland, Marshmellow, and more. You can search by artist, event, year, and/or location.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg">
-				<div class="card">
-					<img src="dankbeats/img/radio.png" class="card-img-top fit" />
-					<div class="card-body">
-						<h5 class="card-title">Live Radio</h5>
-						<p class="card-text">We have handpicked 3 radio station that we enjoy that provide live dj-ing/mixing. You may visit their sites below by clicking on their logo or even listen to them with the player below. You can also send us more stations to add by <a href="https://www.demosjarco.dev/contact.php" target="_self">contacting us</a>.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg">
-				<div class="card">
-					<img src="dankbeats/img/top-logo.png" class="card-img-top fit" />
-					<div class="card-body">
-						<audio controls>
-							<source src="https://ice42.securenetsystems.net/KKFR" type="audio/mpeg">
-						</audio>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg">
-				<div class="card">
-					<img src="dankbeats/img/live-1015-logo-web-header.png" class="card-img-top fit" />
-					<div class="card-body">
-						<audio controls>
-							<source src="http://17813.live.streamtheworld.com:80/KALVFMAAC_SC" type="audio/mpeg">
-						</audio>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg">
-				<div class="card">
-					<img src="dankbeats/img/mx99-4-1024x205.jpeg" class="card-img-top fit" />
-					<div class="card-body">
-						<audio controls>
-							<source src="http://38.107.243.219:8622/;" type="audio/mpeg" />
-						</audio>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="card">
+		<a href="http://live1015phoenix.cbslocal.com/" target="_blank"><img src="img/live-1015-logo-web-header.png" class="logo"></a>
+		<audio controls>
+			<source src="http://17813.live.streamtheworld.com:80/KALVFMAAC_SC" type="audio/mpeg" />
+		</audio>
 	</div>
-	<!-- InstanceEndEditable -->
-	<?php include_once("./parts/footer.html") ?>
+	<div class="card">
+		<a href="http://kmel.iheart.com/" target="_blank"><img src="img/KMEL_Logo.png" class="logo"></a>
+		<audio controls>
+			<source src="https://kmel-fm.akacast.akamaistream.net/7/918/19798/v1/auth.akacast.akamaistream.net/kmel-fm" />
+		</audio>
+	</div>
+	<div class="card">
+		<a href="http://live105.cbslocal.com/" target="_blank"><img src="img/live105blackred225x110.png" class="logo"></a>
+		<audio controls>
+			<source src="http://17963.live.streamtheworld.com:80/KITSFMAAC_SC" type="audio/mpeg" />
+		</audio>
+	</div>
+	<div class="card">
+		<a href="http://mix99radio.com/" target="_blank"><img src="img/mx99-4-1024x205.jpeg" class="logo"></a>
+		<audio controls>
+			<source src="http://38.107.243.219:8622/;" type="audio/mpeg" />
+		</audio>
+	</div>
+</div
 </body>
-<!-- InstanceEnd --></html>
+</html>
