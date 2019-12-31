ef05a16d6167c76c4fcf46e4fdca23ba92fd0a0c
Victor Ilisei <victhebeast@users.noreply.github.com>
2019-12-31 15:59:39 -0700

    Created parts and imported them

---
+++ html/index.php
@@ -0,0 +1,18 @@
+﻿<?php //declare(strict_types=1);
+// HTTP2 Push
+include_once("./parts/preload.php");
+$fileName = basename(__FILE__, '.php');
+?>
+<!doctype html>
+<html>
+<head>
+<?php include_once("./parts/head.php") ?>
+<meta charset="utf-8" />
+<title></title>
+</head>
+<body>
+<?php include_once("./parts/header.php") ?>
+<p>test</p>
+<?php include_once("./parts/footer.php") ?>
+</body>
+</html>
