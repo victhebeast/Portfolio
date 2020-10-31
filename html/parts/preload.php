59321a18a939a84c8ebd675c6e1c69edea6139cb
demosjarco <demosjarco@users.noreply.github.com>
2020-10-30 22:27:05 -0700

    Enabled and updated bootstrap

--- html/parts/preload.php
+++ html/parts/preload.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 header("Link: <https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css>; rel=preload; as=style; crossorigin", false);
-//header("Link: <https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css>; rel=preload; as=style; crossorigin", false);
+header("Link: <https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css>; rel=preload; as=style; crossorigin", false);
 header("Link: <https://fonts.googleapis.com/css?family=Lato:400,700|Roboto>; rel=preload; as=style; crossorigin", false);
 header("Link: </css/main.css>; as=style; rel=preload", false);
 header("Link: </css/light.css>; as=style; rel=preload", false);
 header("Link: </css/dark.css>; as=style; rel=preload", false);
 
 header("Link: <https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js>; as=script; rel=preload; crossorigin", false);
-//header("Link: <https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js>; as=script; rel=preload; crossorigin", false);
+header("Link: <https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.min.js>; as=script; rel=preload; crossorigin", false);
 header("Link: </js/darkMode.js>; as=script; rel=preload", false);
 header("Link: </js/main.js>; as=script; rel=preload", false);
 
